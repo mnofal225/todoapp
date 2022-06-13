58ecaa19bc07c116c8d5558ffe4f91df54434151
mnofal225 <mnofal225@gmail.com>
2022-06-12 22:23:10 -0500

    more clean up and testing out some mui stuff

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { Grid, Box, Input, Button } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
           <h1>The Todo App !</h1>
       </header>
+      <body className="App">
+        <Box>
+          <Input  color='primary' variant="contained" placeholder='What do you want to do?'></Input>
+          <Button variant="contained">ADD</Button>
+
+        </Box>
+      </body>
     </div>
   );
 }
