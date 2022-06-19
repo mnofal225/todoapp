379857eb4949054b72cc46da59dc23ee1434de23
mnofal225 <mnofal225@gmail.com>
2022-06-19 15:57:27 -0500

    Adding the methods to the end points

--- server/server.js
+++ server/server.js
@@ -6,12 +6,18 @@ const connectDB = require("./config/db"); //added
 
 const app = express();
 
+// routes
+const todo = require("./routers/todo") // added
+
 // connect database
 connectDB();//added
 
 // initialize middleware
 app.use(express.json({ extended: false }));
 app.get("/", (req, res) => res.send("Server up and running"));
+
+// use the routes
+app.use("/api/todo", todo) 
 
 // setting up port
 const PORT = process.env.PORT || 8000;
